0cc72651e1f3a45a070b52ae8a3a94503eac8f30
Cocepter Bosibori <bosiboriconcepter501@gmail.com>
2026-04-20 17:35:39 +0300

    Create: component files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer () {
+    return (
+        <footer>
+            <p>&copy; 2026 IYF Alumni Connect. All Rights Reserved.</p>
+        </footer>
+    )
+
+}
+
+export default Footer;
---
+++ src/components/Postcard.jsx
@@ -0,0 +1,11 @@
+function PostCard() {
+    return (
+        <div className="post-card">
+            <h3>Post Title</h3>
+            <p>Post content goes here.</p>
+            <span>Posted on April 2, 2026</span>
+        </div>
+    )
+}
+
+export default PostCard;
