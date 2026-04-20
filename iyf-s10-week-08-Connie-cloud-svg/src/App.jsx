6cd9a0a434a81c6a5edf9bcec6f8e00e1a945810
Cocepter Bosibori <bosiboriconcepter501@gmail.com>
2026-04-20 12:04:47 +0300

    Add: Header.jsx file to components

--- iyf-s10-week-08-Connie-cloud-svg/src/App.jsx
+++ iyf-s10-week-08-Connie-cloud-svg/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -41,6 +42,7 @@ function App() {
           Count is {count}
         </button>
       </section>
+
 
       <div className="ticks"></div>
 
@@ -158,5 +160,6 @@ function Greeting(){
 
   return <p><strong>{message}</strong></p>;
 }
+
 
 export default App
