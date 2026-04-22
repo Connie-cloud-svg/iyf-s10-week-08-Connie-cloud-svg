acf21bee49a0e648cb7f3f4f5b822f444617db10
Cocepter Bosibori <bosiboriconcepter501@gmail.com>
2026-04-22 18:44:30 +0300

    Add: footer, sidebar, button and postcard to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import PostCard from "./Components/Postcard";
+import Sidebar from "./Components/Sidebar";
+
 function App() {
   const name = "Connie";
   const today = new Date();
@@ -12,16 +17,20 @@ function App() {
 
   return (
     <section className="App">
-      <h1>Hello👋, I'm {name}. Welcome to my app.</h1>
+      <h1>Hello👋, I'm {name}.</h1>
       <p>I am a developer learning React at IYF Weekend Academy Season 10.</p>
       <p>
         I enjoy building things and solving problems one component at a time.
       </p>
       <p>This is my Week 8 project: .</p>
+      <Header />
+      <Sidebar />
+      <PostCard />
       <div className="Date-Time-Message">
-        <p>📅 Today is {today.toDateString()}</p>
+        <p>📅{today.toDateString()}</p>
         <p>{timeMessage}</p>
       </div>
+      <Footer />
     </section>
   );
 }
