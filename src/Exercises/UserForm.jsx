f320436f0213a4455346b0cf215075699e86e4b9
Cocepter Bosibori <bosiboriconcepter501@gmail.com>
2026-04-24 19:38:18 +0300

    Add: ContactForm.jsx

---
+++ src/Exercises/UserForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function ContactForm () {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form data:', formData)
+        setFormData({name: '', email: '', message: ''});
+    };
+
+    return (
+        
+    )
+}
