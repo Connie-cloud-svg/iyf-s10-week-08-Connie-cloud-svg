e3a487a707206c544a9fd8f9c6635f93e7cf39b9
Connie <bosiboriconcepter501@gmail.com>
2026-05-05 12:01:01 +0300

    Remove:  components and exercises folder

--- src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p>&copy; 2026 Community Hub. All Rights Reserved.</p>
-        </footer>
-    );
-}
-
-export default Footer;
